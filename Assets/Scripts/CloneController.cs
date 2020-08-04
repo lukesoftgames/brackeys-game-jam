79f1fe09047e732e1fafbc344efed9f1a92f6e93
Benjamin Kershenabum <benji.kershenbaum@gmail.com>
2020-08-05 00:28:39 +0100

    Round loop manager and clones

---
+++ Assets/Scripts/CloneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneController : MonoBehaviour {
+    private List<PointInTime> pointsInTime;
+    private int cloneID;
+    private int posIndex;
+    private float t;
+    [SerializeField] private float posUpdateTime = 1f;
+
+    private void Awake() {
+        posIndex = 0;
+        pointsInTime = new List<PointInTime>();
+        GameEvents.current.onSendPointsInTime += SetClonePositionInTime;
+        GameEvents.current.onRoundEnd += ResetClone;
+    }
+
+    private void ResetClone(int cloneID) {
+        posIndex = 0;
+    }
+
+    private void SetClonePositionInTime(int curCloneID, List<PointInTime> curPointInTime) {
+        if (curCloneID == cloneID) {
+            pointsInTime = curPointInTime;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (pointsInTime.Count -1 > posIndex) {
+            Debug.Log("initial" + pointsInTime[posIndex].rotation);
+            Debug.Log("next" + pointsInTime[posIndex+1].rotation);
+            t += Time.deltaTime / posUpdateTime;
+            transform.position = Vector3.Lerp(pointsInTime[posIndex].position, pointsInTime[posIndex+1].position, t);
+            transform.rotation = Quaternion.Lerp(pointsInTime[posIndex].rotation, pointsInTime[posIndex + 1].rotation, t);
+            if (t > 1) {
+                t = 0;
+                posIndex += 1;
+            }
+
+        }
+    }
+
+    public void setCloneID(int curCloneID) {
+        cloneID = curCloneID;
+    }
+}
