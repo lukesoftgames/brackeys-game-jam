c6d60be0a137c0368b5cb97c4bf76c97e58bb5d0
freddiejbawden <freddiejbawden@gmail.com>
2020-08-05 11:18:15 +0100

    Level Test

---
+++ Assets/GroundTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTree : MonoBehaviour
+{
+    public LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up * -1), out hit, Mathf.Infinity, layerMask))
+        {
+            transform.position = hit.point;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
